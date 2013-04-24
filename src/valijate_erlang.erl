958c8a7763276bb24966cf8b60eac4e168c61281
Erik Søe Sørensen <ess@trifork.com>
2013-04-24 16:05:01 +0200

    Add Erlang term validator.

---
+++ src/valijate_erlang.erl
@@ -0,0 +1,198 @@
+-module(valijate_erlang).
+
+-export([validate/2, error_to_english/1]).
+
+-type condition_description() :: string().
+-type erlang_shallow_type() :: atom | integer | float | number | binary | boolean | list | tuple.
+-type field_name() :: atom().
+-type proplist_field_spec() ::
+        {atom(), type_spec()}
+      | {opt, atom(), type_spec(), Default::_}
+      | {keep_rest, fun(([_])-> _)}.
+-type type_spec() ::
+        atom | integer | float | number | binary | boolean | list | tuple | string
+      | {list, type_spec}
+      | {proplist, [proplist_field_spec()]}
+      | {satisfy, fun((_)->_), condition_description()}
+      | {convert, fun((_)->_), condition_description()}
+      | {either, [type_spec()]}                 % TODO: Planned
+      | {pipeline, [type_spec()]}.              % TODO: Planned
+
+-type validation_error() ::
+        {bad_type_spec, _}
+      | {wrong_type, Value::_, Found::erlang_shallow_type(), Expected::erlang_shallow_type()}
+      | {unpure_list, Tail::_, Found::erlang_shallow_type()}
+      | {missing_field, field_name()}
+      | {superfluous_fields, [field_name()]}
+        %% TODO: Add case for converter errors.
+      | {does_not_satisfy, Value::_, condition_description()}.
+
+-type term_path() :: [binary() | integer()].
+
+%%%========== Main validator ========================================
+
+%%% Validate and reform an Erlang term.
+%%%
+-spec validate/2 :: (_Term, _Type :: type_spec()) ->
+                            {ok, _} | {validation_error, term_path(), validation_error()}.
+
+validate(Value, Type) ->
+    try {ok, validate(Value, Type, [])}
+    catch throw:{validation_error,_,_}=VErr ->
+            VErr
+    end.
+
+%% Simple types:
+validate(V, atom, _)    when is_atom(V)  -> V;
+validate(V, number, _)  when is_number(V)  -> V;
+validate(V, integer, _) when is_integer(V)  -> V;
+validate(V, float, _)   when is_float(V)  -> V;
+validate(V, binary, _)  when is_binary(V)  -> V;
+validate(V, boolean, _) when is_boolean(V) -> V;
+validate(V=null, null, _) -> V;
+validate(V, string, RevPath)  ->
+    case validate(V, {list,integer}) of
+        {ok,R} -> R;
+        {validation_error,_,_} ->
+            type_error(string, V, RevPath)
+    end;
+validate(V, Spec, RevPath) when is_atom(Spec) ->
+    type_error(Spec, V, RevPath);
+%% Lists:
+validate(V, {list, ElemType}, RevPath) ->
+    if is_list(V) ->
+            validate_list(V, ElemType, RevPath);
+       true ->
+            type_error(list, V, RevPath)
+    end;
+%% Objects:
+%% Proplists:
+validate(Fs, {proplist, FieldTypes}, RevPath) when is_list(Fs),
+                                                   is_list(FieldTypes) ->
+    validate_proplist(Fs, FieldTypes, RevPath);
+validate(V, {proplist, FieldTypes}, RevPath) when is_list(FieldTypes) ->
+    type_error(proplist, V, RevPath);
+%% Advanced type specs:
+validate(V, {satisfy, F, CondDescr}, RevPath) when is_function(F,1) ->
+    case F(V) of
+        true -> V;
+        false -> validation_error(RevPath, {does_not_satisfy, V, CondDescr})
+    end;
+validate(V, {convert, F, CondDescr}, RevPath) when is_function(F,1) ->
+    case F(V) of
+        {ok,Result} -> Result;
+        {error,_Err} -> validation_error(RevPath, {does_not_satisfy, V, CondDescr})
+    end;
+validate(_, BadSpec, RevPath) ->
+    validation_error(RevPath, {bad_type_spec, BadSpec}).
+
+
+%%%---------- Lists ----------------------------------------
+validate_list(List, ElemType, RevPath) ->
+    validate_list(List, ElemType, RevPath, 0).
+
+validate_list([], _ElemType, _RevPath, _Idx) -> [];
+validate_list([H|T], ElemType, RevPath, Idx) ->
+    [validate(H, ElemType, [Idx|RevPath])
+     | validate_list(T, ElemType, RevPath, Idx+1)];
+validate_list(Tail, _ElemType, RevPath, _Idx) ->
+    validation_error(RevPath,
+                     {unpure_list, Tail, shallow_type(Tail)}).
+
+
+validate_proplist(Fs, FieldTypes, RevPath) ->
+    {FVs,RestObj} =
+        lists:mapfoldl(fun(FieldSpec, Obj) ->
+                               validate_proplist_field(Obj, FieldSpec, RevPath)
+                       end,
+                       Fs,
+                       FieldTypes),
+    if RestObj==[] ->
+            list_to_tuple(FVs);
+       true ->
+            ExtraFieldNames = [N || {N,_} <- RestObj],
+            validation_error(RevPath, {superfluous_fields, ExtraFieldNames})
+    end.
+
+%%%---------- Objects ----------------------------------------
+validate_proplist_field(Obj, {FName, FSpec}, RevPath) when is_atom(FName) ->
+    case lists:keytake(FName, 1, Obj) of
+        {value, {_,V}, Rest} ->
+            {validate(V, FSpec, [FName|RevPath]), Rest};
+        false ->
+            validation_error(RevPath, {missing_field, FName})
+    end;
+validate_proplist_field(Obj, {opt, FName, FSpec, Default}, RevPath)
+  when is_atom(FName) ->
+    case lists:keytake(FName, 1, Obj) of
+        {value, {_,V}, Rest} ->
+            {validate(V, FSpec, [FName|RevPath]), Rest};
+        false ->
+            {Default, Obj}
+    end;
+validate_proplist_field(Obj, {keep_rest, F}, _RevPath) ->
+    {F(Obj), []}.
+%% TODO: Handle typespec error
+
+%%%========== Error reporting ========================================
+type_error(ExpectedType, Value, RevPath) ->
+    validation_error(RevPath,
+                     {wrong_type, Value, shallow_type(Value), ExpectedType}).
+
+
+validation_error(RevPath, ErrorDetail) ->
+    throw({validation_error,
+           lists:reverse(RevPath),
+           ErrorDetail}).
+
+%%% Determine the (shallow) type of a JSON term.
+-spec shallow_type/1 :: (_) -> erlang_shallow_type().
+shallow_type(V) when is_atom(V)    -> atom;
+shallow_type(V) when is_number(V)  -> number;
+shallow_type(V) when is_binary(V)  -> binary;
+shallow_type(V) when is_boolean(V) -> boolean;
+shallow_type(V) when is_list(V)    -> list;
+shallow_type(V) when is_tuple(V)   -> tuple;
+shallow_type(V) when is_reference(V) -> reference;
+shallow_type(V) when is_pid(V)     -> pid;
+shallow_type(V) when is_bitstring(V) -> bitstring;
+shallow_type(V) when is_function(V)-> function.
+
+%%% Convert a validation-error output to a human readable description
+%%% of the error.
+-spec error_to_english/1 :: (validation_error()) -> iolist().
+error_to_english({validation_error, Path, Details}) ->
+    PathTxt = path_to_string(Path),
+    DetailText =
+        case Details of
+            {bad_type_spec, Spec} ->
+                io_lib:format("The type spec ~p is malformed", [Spec]);
+            {wrong_type, Value, Found, Expected} ->
+                if is_atom(Value);
+                   is_number(Value) ->
+                        io_lib:format("Value ~p has type ~p, but ~p was expected", [Value, Found, Expected]);
+                   true ->
+                        io_lib:format("Value has type ~p, but ~p was expected", [Found, Expected])
+                end;
+            {unpure_list, _Tail, TailType} ->
+                io_lib:format("List has unpure end of type ~p", [TailType]);
+            {missing_field, FieldName} ->
+                io_lib:format("The object is missing field \"~s\"", [FieldName]);
+            {superfluous_fields, FieldNames} ->
+                "The object has superfluous fields: "
+                    ++ string:join([["\"", FN, "\""] || FN <- FieldNames],
+                                   ", ");
+            {does_not_satisfy, Value, CondDescr} ->
+                io_lib:format("The value does not satisfy ~s: ~p\n",
+                              [CondDescr, Value])
+        end,
+    lists:flatten(io_lib:format("At path ~s : ~s", [PathTxt, DetailText])).
+
+path_to_string([]) -> "<root>";
+path_to_string(Path) ->
+    [if is_integer(P) ->
+             ["[", integer_to_list(P), "]"];
+        is_binary(P) ->
+             [".", P]
+     end
+     || P <- Path].
---
+++ test/valijate_erlang_tests.erl
@@ -0,0 +1,82 @@
+-module(valijate_erlang_tests).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(LOG_TRACE(Body),
+        try (Body)
+        catch Cls:Err ->
+                Trace = erlang:get_stacktrace(),
+                error_logger:error_msg("Exception: ~p:~p\n** Trace: ~p\n",
+                                       [Cls, Err, Trace]),
+                erlang:raise(Cls,Err,Trace)
+        end).
+
+simple_type_test() ->
+    ?LOG_TRACE(begin
+    TVs = [{atom, xyz},
+           {number, 123.45},
+           {string, "Some string"},
+           {binary, <<"Some string">>},
+           {boolean, true},
+           {boolean, false},
+           {list, [x]},
+           {tuple, {}},
+           {tuple, {a,2,"c"}},
+           {proplist, [{foo, "foo"}, {bar, 123.45}]},
+           {proplist, []}],
+
+    %% Matrix check: Value from V1, spec from T2:
+    [begin
+         TypeOK = if
+                      T2=:=T1                 -> true;
+                      %% Handle subcasing:
+                      T2==atom,   T1==boolean -> true;
+                      T2==number, T1==integer -> true;
+                      T2==number, T1==float   -> true;
+                      T2==list,   T1==string  -> true;
+                      T2==list,   T1==proplist-> true;
+
+                      true                    -> false
+                  end,
+         if TypeOK ->
+                 ?assertEqual({ok,V1}, valijate_erlang:validate(V1, T2));
+            not TypeOK ->
+                 TSeen = case T1 of
+                             string -> list;
+                             proplist -> list;
+                             boolean -> atom;
+                             _ -> T1
+                         end,
+                 ?assertEqual({validation_error, [], {wrong_type, V1, TSeen, T2}},
+                              valijate_erlang:validate(V1, T2))
+         end
+     end
+     || {T1,V1} <- TVs,
+        {T2,_V2} <- TVs,
+        T2 /= list, T2 /= tuple, T2 /= string, T2 /= proplist % Only simple types.
+    ]
+    end).
+
+string_happy_case_test() ->
+    ?assertEqual({ok, "Thith ith a tetht"},
+                 valijate_erlang:validate("Thith ith a tetht", string)).
+
+string_bad_case_test() ->
+    ?assertEqual({validation_error, [], {wrong_type, [xyz], list, string}},
+                 valijate_erlang:validate([xyz], string)),
+    ?assertEqual({validation_error, [], {wrong_type, "Unpure: " ++ x, list, string}},
+                 valijate_erlang:validate("Unpure: " ++ x, string)).
+
+proplist_happy_case_test() ->
+    ?LOG_TRACE(begin
+    Spec = {proplist, [{fred,   binary},
+                       {george, string},
+                       {ginny,  boolean}
+                      ]},
+    Object = [{george, "Hello, Harry!"},
+              {fred, <<"Hello, Harry!">>},
+              {ginny, true}],
+    ?assertEqual({ok, {<<"Hello, Harry!">>, "Hello, Harry!", true}},
+                 valijate_erlang:validate(Object, Spec)) end).
